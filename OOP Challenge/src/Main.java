7fb44bd404fcb0002eb943edcbd5abbfb9f9e678
Nemesis369 <457akshat@gmail.com>
2025-01-13 21:40:49 +0530

    OOP Challenge

--- OOP Challenge/src/Main.java
+++ OOP Challenge/src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
-    
+
     public static void main(String[] args) {
-        
+        // Item fries = new Item("sides", "Fries", 3.0);
+        // fries.printList();
+        // fries.setSize("Large");
+        // fries.printList();
+
+        // Item cheese = new Item("topping", "Cheese", 1.0);
+        // cheese.printList();
+
+        // Item avocado = new Item("Topping", "avocado", 1.50);
+        // avocado.printList();
+
+        // Burger burger = new Burger("regular", 4.00);
+        // burger.addToppings("HAM", "CHEESE", "AVOCADO");
+        // burger.printList();
+
+        MealOrder meal = new MealOrder("Sprite", "Tenders", "CheeseBurger");
+
+        meal.addToppings("HAM", "CHEESE", "AVOCADO");
+        meal.changeDrinkSize("LARGE");
+        meal.printItemizedList();
     }
 }
--- OOP Challenge/src/MealOrder.java
+++ OOP Challenge/src/MealOrder.java
@@ -7,21 +7,23 @@ public class MealOrder {
     public MealOrder() {
         this("Coke", "Fries", "Cheeseburger");
     }
+
     public MealOrder(String drinkName, String sideName, String burgerName) {
         drink = new Item("drink", drinkName, 5.0);
         sideItem = new Item("sides", sideName, 1.50);
-        burger = new Burger("burger", 3.0);
+        burger = new Burger(burgerName, 3.0);
     }
 
     public void addToppings(String extra1, String extra2, String extra3) {
         burger.addToppings(extra1, extra2, extra3);
     }
+
     public void changeDrinkSize(String size) {
         drink.setSize(size);
     }
 
     public double getTotalPrice() {
-        return drink.getAdjustedPrice() + sideItem.getAdjustedPrice() + burger.getTotalPrice();
+        return drink.getAdjustedPrice() + sideItem.getAdjustedPrice() + burger.getAdjustedPrice();
     }
 
     public void printItemizedList() {
@@ -29,6 +31,6 @@ public class MealOrder {
         drink.printList();
         sideItem.printList();
         System.out.println("-".repeat(30));
-        System.out.printf("TOTAL :  %d", getTotalPrice());
+        System.out.println(String.format("TOTAL :  %.2f", getTotalPrice()).indent(8));
     }
 }
